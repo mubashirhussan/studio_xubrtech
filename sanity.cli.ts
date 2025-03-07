b3bfea986e6cce8fda699e6263def11878d41aa3
Mubashir <mubashir.hussan@xevensolutions.com>
2025-03-08 02:07:30 +0500

    update environment

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -2,8 +2,12 @@ import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: '3aauiptw',
-    dataset: 'production'
+    projectId: process.env.SANITY_STUDIO_PROJECT_ID,
+    dataset: process.env.SANITY_STUDIO_DATASET,
+  },
+  studioHost: process.env.SANITY_STUDIO_HOSTNAME,
+  project: {
+    basePath: '/studio',
   },
   /**
    * Enable auto-updates for studios.
--- sanity.config.ts
+++ sanity.config.ts
@@ -5,7 +5,7 @@ import {schemaTypes} from './schemaTypes'
 import {codeInput} from '@sanity/code-input'
 
 export default defineConfig({
-  name: 'default',
+  name: 'Xubrtech',
   title: 'studio_xubrtech_backend',
   projectId: process.env.SANITY_STUDIO_PROJECT_ID as string,
   dataset: process.env.SANITY_STUDIO_DATASET || 'development',
