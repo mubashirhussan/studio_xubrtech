ac4d0b0e9c4e850d65ede9ee8497bfccf8f940d0
mubashirhussan <mubashirhussan93@gmail.com>
2025-03-11 16:43:52 +0500

    contact form have been added

---
+++ schemaTypes/contactForm.ts
@@ -0,0 +1,35 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'contactForm',
+  title: 'Contact Form Messages',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    }),
+    defineField({
+      name: 'email',
+      title: 'Email',
+      type: 'string',
+    }),
+    defineField({
+      name: 'phone',
+      title: 'Phone Number',
+      type: 'string',
+    }),
+    defineField({
+      name: 'message',
+      title: 'Message',
+      type: 'text',
+    }),
+    defineField({
+      name: 'createdAt',
+      title: 'Created At',
+      type: 'datetime',
+      initialValue: () => new Date().toISOString(),
+    }),
+  ],
+})
